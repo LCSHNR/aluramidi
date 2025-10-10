68f85bba4003aa11522841c50d8573e99849ac4c
LUCAS HENRIQUE DE OLIVEIRA <henrique.oliveira03@escola.pr.gov.br>
2025-10-10 10:44:44 +0000

    onkeydown

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSomPom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+
+    if(elemento && elemento.localName === 'audio'){
+        elemento.play;
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
 
 const listadeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listadeTeclas.length; contador ++){
+
+    const tecla = listadeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function (){
+        tocaSomPom(idAudio);
+    }
+    tecla.onkeydown = function (evento){
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+    }
+}
 listadeTecla[0].onclick = tocaSomPom;
