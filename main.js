88c7dfc90e068c77de8e97bb7926c376a58472f0
LUCAS HENRIQUE DE OLIVEIRA <henrique.oliveira03@escola.pr.gov.br>
2025-10-12 22:22:31 +0000

    correção do js

--- main.js
+++ main.js
@@ -1,29 +1,39 @@
-function tocaSomPom(seletorAudio){
+function tocaSom (seletorAudio) {
     const elemento = document.querySelector(seletorAudio);
 
-    if(elemento && elemento.localName === 'audio'){
-        elemento.play;
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
     }
     else {
+        //alert('Elemento não encontrado');
         console.log('Elemento não encontrado ou seletor inválido');
     }
+
 }
 
-const listadeTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-for (let contador = 0; contador < listadeTeclas.length; contador ++){
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-    const tecla = listadeTeclas[contador];
+    const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`;
+    const idAudio = `#som_${instrumento}`; //template string
 
-    tecla.onclick = function (){
-        tocaSomPom(idAudio);
+    tecla.onclick = function () {
+        tocaSom(idAudio);
     }
-    tecla.onkeydown = function (evento){
-        if (evento.code === 'Space' || evento.code === 'Enter'){
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
             tecla.classList.add('ativa');
         }
+
     }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
 }
-listadeTecla[0].onclick = tocaSomPom; 
